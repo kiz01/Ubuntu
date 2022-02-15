603d8f7b7fb60aa037e80ceba3f4e27957b6d16e
Kshitiz Bhushan <kshitizbhushan01@gmail.com>
2022-02-16 01:21:06 +0530

    Update ubuntu_post_install.sh

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -3,5 +3,5 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sudo apt install vlc gimp blender kazam dconf-editor rhythmbox telegram-desktop transmission -y
+sudo apt install vlc gimp blender kazam dconf-editor rhythmbox telegram-desktop gnome-tweaks neofetch python3-tk python-is-python3 nautilus-image-converter -y
 
